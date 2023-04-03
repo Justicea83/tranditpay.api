ad7ee1882c322623d27fb3a002fc22403486d84c
Justice Arthur <justicea83@gmail.com>
2023-04-03 15:11:42 +0000

    fix pending transaction

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\Auth\PruneOtps;
+use App\Console\Commands\Payments\ProcessPendingRequests;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command(ProcessPendingRequests::class)->everyFiveMinutes()->withoutOverlapping()->runInBackground();
         $schedule->command(PruneOtps::class)->everyFiveMinutes()->withoutOverlapping()->runInBackground();
         $schedule->command('passport:purge')->hourly();
         $schedule->command('queue:work --tries=3 --timeout=3000')->everyMinute()->withoutOverlapping();
